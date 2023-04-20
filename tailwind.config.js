2aba52be6cd2488bde5245ff2190913fea22f8e0
waqas662218 <128673557+waqas662218@users.noreply.github.com>
2023-04-20 11:26:18 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1152px',
+      }
+    },
+    fontFamily: {
+      'display': ['Poppins', 'sans-serif'],
+      'body': ['Roboto', 'sans-serif'],
+       'urdu': ['Noto Nastaliq Urdu'],
+    },
+    extend: {
+      fontSize: {
+        '2xs': ['0.625rem', '0.75rem'], // 10px
+        '3xs': ['0.5rem', '0.625rem'], // 8px
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
