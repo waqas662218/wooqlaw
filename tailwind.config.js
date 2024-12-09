8b073931ed86a04cd9c4294f136b286183fa384c
waqas <wqsmalik777@gamil.com>
2024-12-09 15:07:20 +0500

    merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/**/*.{html,js}"],
+export default {
   theme: {
-    container: {
-      center: true,
-      padding: '1rem',
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1152px',
-      }
-    },
-    fontFamily: {
-      'display': ['Poppins', 'sans-serif'],
-      'body': ['Roboto', 'sans-serif'],
-       'urdu': ['Noto Nastaliq Urdu'],
-    },
     extend: {
-      fontSize: {
-        '2xs': ['0.625rem', '0.75rem'], // 10px
-        '3xs': ['0.5rem', '0.625rem'], // 8px
-      },
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
-      }
+        primary: {
+          50: '#f1f5fc',
+          100: '#e5ecfa',
+          200: '#d1dbf4',
+          300: '#b4c3ed',
+          400: '#96a4e3',
+          500: '#7c87d8',
+          600: '#5b5fc7',
+          700: '#5152b1',
+          800: '#44468f',
+          900: '#3c3f73',
+          950: '#232443',
+        },
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+  plugins: [],
 }
